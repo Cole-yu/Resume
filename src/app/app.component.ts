82f3e33d57bea086f392cba480e7157e9adf9e3a
cole-yu <1032577646@qq.com>
2017-12-26 23:39:37 +0800

    增加了动画

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {appRoutes} from './app.router';
 import {MonthDetailComponent} from './components/month-detail/month-detail.component';
 import {HomeComponent} from './components/home/home.component';
@@ -10,19 +10,69 @@ import {HomeComponent} from './components/home/home.component';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   constructor(){
   }
 
+  public sysTime="";
+  public title="601室费用档案管理系统";
+
+  //组件被创建时生成
+  ngOnInit() {
+    this.settime()
+  }
+
+  public Time(){
+     var date=new Date();
+    //console.log(typeof(date.getFullYear().toString()));         //数字2017转为文字
+     let year=date.getFullYear().toString();
+     let month=date.getMonth().toString();
+     let day=date.getDate().toString();
+     this.sysTime=year+month+day;
+     console.log(this.sysTime);
+  }
+
+  public settime(){
+    setInterval(this.Time,1000);
+  }
+
   public navbar=[
-    {id:0,name:"Home",status:"0",link:"/home"},
-    {id:1,name:"Product",status:"0",link:"/pro"},
-    {id:2,name:"Reusume",status:"0",link:"/resume"},
-    {id:3,name:"Knowledge",status:"0",link:"/know"},
-    {id:3,name:"MonthDetail",status:"1",link:"/month"},
+    {id:0,name:"Home",status:"0",childStatus:false,link:"/home",children:[
+        {id:10,name:"单位管理",status:"0",link:"./unit"},
+        {id:20,name:"单位类别管理",status:"0",link:"./unitType"}
+      ]
+    },
+    {id:1,name:"Product",status:"0",childStatus:false,link:"/pro",children:[
+      {id:11,name:"单位管理",status:"0",link:"./unit"},
+      {id:12,name:"单位类别管理",status:"0",link:"./unitType"}
+      ]
+    },
+    {id:2,name:"Reusume",status:"0",childStatus:false,link:"/resume",children:[
+        {id:21,name:"单位管理",status:"0",link:"./unit"},
+        {id:22,name:"单位类别管理",status:"0",link:"./unitType"}
+      ]
+    },
+    {id:3,name:"Knowledge",status:"0",childStatus:false,link:"/know",children:[
+        {id:31,name:"单位管理",status:"0",link:"./unit"},
+        {id:32,name:"单位类别管理",status:"0",link:"./unitType"}
+      ]
+    },
+    {id:3,name:"MonthDetail",status:"1",childStatus:false,link:"/month",children:[]},
   ]
 
+  // 菜单栏开关按钮
+  public show(index:number){
+    if(this.navbar[index].childStatus==true){
+      this.navbar[index].childStatus=false;
+    }
+    else {
+      for(let i=0;i<this.navbar.length;i++){
+        this.navbar[i].childStatus=false;
+      }
+      this.navbar[index].childStatus=true;
+    }
+  }
 
 
 
--- src/app/components/knowledge/knowledge.component.ts
+++ src/app/components/knowledge/knowledge.component.ts
@@ -54,4 +54,24 @@ export class KnowledgeComponent implements OnInit {
       this.el[0].checked=false;
     }
   }
+
+  //模态框
+  public model(){
+    var modal = document.getElementById('myModal');
+    // 获取图片模态框，alt 属性作为图片弹出中文本描述
+    var img = document.getElementById('myImg');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    modal.style.display = "block";
+    // modalImg.src = img.src;
+  }
+  //关闭事件
+  public close() {
+    // 获取 <span> 元素，设置关闭模态框按钮
+    var span = document.getElementsByClassName("close")[0];
+    // 点击 <span> 元素上的 (x), 关闭模态框
+    var modal = document.getElementById('myModal');
+    modal.style.display = "none";
+  }
+
 }
